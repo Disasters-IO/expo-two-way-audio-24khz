ed6faa7dbf00518e7bbc9e021cb462b71d993008
Browsify <browsifyshopping@gmail.com>
2025-08-06 17:13:08 +0500

    made byte freq data sync

--- ios/ExpoTwoWayAudioModule.swift
+++ ios/ExpoTwoWayAudioModule.swift
@@ -56,7 +56,7 @@ public class ExpoTwoWayAudioModule: Module {
             return true
         }
 
-        AsyncFunction("getByteFrequencyData") { () -> [UInt8] in
+        Function("getByteFrequencyData") { () -> [UInt8] in
             guard let audioEngine = self.audioEngine else {
                 print("AudioEngine not initialized")
                 return []
